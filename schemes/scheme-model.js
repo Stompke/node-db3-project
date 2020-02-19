a83e84fb502f4883757513de20a615f2d5b2c178
Shawn Tompke <shawntompke@Shawns-MacBook-Pro.local>
2020-02-19 15:01:42 -0800

    All db helper methods work correctly. MVP completed

---
+++ schemes/scheme-model.js
@@ -0,0 +1,45 @@
+const db = require('../data/dbConfig');
+
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find(){
+   return db('schemes');
+}
+
+function findById(id){
+    return db('schemes')
+    .where({ id })
+}
+
+function findSteps(id){
+    return db('steps as st')
+    .join("schemes as sc", "st.scheme_id", "sc.id")
+    .select("st.id","sc.scheme_name","st.step_number","st.instructions")
+    .where({ scheme_id: id})
+    .orderBy('step_number')
+}
+
+function add(data){
+    return db('schemes')
+    .insert(data, 'id')
+}
+
+function update(changes, id){
+    return db('schemes')
+    .where({ id })
+    .update(changes)
+}
+
+function remove(id){
+    return db('schemes')
+    .where({ id })
+    .del()
+}
